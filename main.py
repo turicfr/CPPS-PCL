2df0b3b747b6e09ad4ba1bf5acf9acd9f7463bd7
relrelb <relrelbachar@gmail.com>
2019-11-02 18:53:10 +0200

    Add COMMAND_HANDLERS

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
+import functools
+
 from client import GenericClient
+
+COMMAND_HANDLERS = {}
+
+def command_handler(name):
+    def _command_handler(func):
+        @functools.wraps(func)
+        def _func(client, *args):
+            try:
+                return func(client, *args)
+            except TypeError:
+                return None
+        COMMAND_HANDLERS[name] = _func
+        return _func
+    return _command_handler
+
+@command_handler("id")
+def handle_id(client):
+    return client.id
 
 def main():
     client = GenericClient("45.76.128.185", 6112)
@@ -8,13 +28,23 @@ def main():
     while True:
         client.update()
         try:
-            command = input("> ")
+            command = input("> ").split()
         except KeyboardInterrupt:
             print()
             continue
         except EOFError:
             break
-        print(command)
+        if not command:
+            continue
+        name, args = command[0], command[1:]
+        if name not in COMMAND_HANDLERS:
+            print(f"Unknown command: {name}")
+            continue
+        output = COMMAND_HANDLERS[name](client, *args)
+        if output is None:
+            print("Error")
+            continue
+        print(output)
 
 if __name__ == "__main__":
     main()
